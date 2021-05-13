508d5c2da34b94f91cb5f1a1b454a109f6804b0b
Matthew Luedtke <MLuedtke@tql.com>
2021-05-13 14:32:28 -0400

    added delete

--- insert-update.sql
+++ insert-update.sql
@@ -6,3 +6,6 @@ Update Student set
 	MajorId = (select major.id from Major where major.Description = 'Accounting'), 
 	GPA = (3.51)
 	where student.Firstname = 'Noah' and student.Lastname = 'Phence';
+
+delete from student
+	where student.Firstname = 'Noah' and student.Lastname = 'Phence';
